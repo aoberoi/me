da078a4256b5b3ef1654bad47df2a6fe07676119
Ankur Oberoi <aoberoi@gmail.com>
2021-04-25 19:10:33 -0400

    update favicons
    see: https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,12 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <link rel="icon" href="/favicon.ico" />
+          {/* TODO: use webpack image loader to generate cache busting filenames (in the manifest file too) */}
+          {/* TODO: update the icon with a transparent background */}
+          <link rel="icon" href="/icon.v1.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.v1.png" />
         </Head>
         <body>
           <Main />

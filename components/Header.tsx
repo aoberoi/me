83fdbc54de3e90f8e325f717bc9bb1c87474250c
Ankur Oberoi <aoberoi@gmail.com>
2022-01-26 19:33:00 -0800

    upgrades to tailwindcss@3

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="bg-gray-800 mb-6">
+    <div className="bg-zinc-800 mb-6">
       <header className="max-w-screen-lg mx-auto px-4 py-6">
         <h1 className="text-white font-banner text-4xl">
           <Link href="/">Ankur Oberoi</Link>

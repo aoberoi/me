2940480f1d1f2eb9ea2b4e89cb8dd125023cacd4
Ankur Oberoi <aoberoi@gmail.com>
2022-01-28 14:56:21 -0800

    Add links to new pages in navigation (unstyled)

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,20 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div className="bg-zinc-800 mb-6">
-      <header className="max-w-screen-lg mx-auto px-4 py-6">
-        <h1 className="text-white font-banner text-4xl">
+      <header className="max-w-screen-lg mx-auto px-4 py-6 text-white">
+        <h1 className="font-banner text-4xl">
           <Link href="/">Ankur Oberoi</Link>
         </h1>
+        <nav>
+          <ul className="flex">
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/posts">Posts</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );

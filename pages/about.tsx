765a09261ad065730541513faaae8a2fa672ccb4
Ankur Oberoi <aoberoi@gmail.com>
2022-01-28 15:00:53 -0800

    Adds about page with some placeholder content

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <Layout title="About">
+      <section className="max-w-screen-lg mx-auto px-4">
+        <h2 className="text-2xl font-banner my-2">About</h2>
+      </section>
+    </Layout>
+  );
+}

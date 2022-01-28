4bd6d56017a60da97ccfde0caf741af6153a8701
Ankur Oberoi <aoberoi@gmail.com>
2022-01-28 15:02:05 -0800

    Adds a posts page that lists all posts in chronological order.
    This is mostly cribbed from the home page. It's likely that the home page
    design will change significantly so its lift and shift.

---
+++ pages/posts.tsx
@@ -0,0 +1,44 @@
+import type { GetStaticProps } from 'next';
+import { Post, getRecentPosts } from '../lib/post';
+import { formatInTimeZone } from '../lib/date';
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+interface PostsProps {
+  posts: Post[];
+}
+
+
+export default function PostIndex({ posts }: PostsProps) {
+  return (
+    <Layout title="Posts">
+      <section className="max-w-screen-lg mx-auto px-4">
+        <h2 className="text-2xl font-banner my-2">Posts</h2>
+        <table className="w-full mx-0 mt-0 mb-4 p-0">
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.slug}>
+                <td className="pr-4 py-3 border-b border-gray-200/10 text-right opacity-40">{formatInTimeZone(post.date * 1000, 'MMMM do, R', 'UTC')}</td>
+                <td className="pl-4 py-3 border-b border-gray-200/10 text-left">
+                  <Link href={`/p/${post.slug}`} passHref={true}>
+                    <a className="text-red-700 underline">{post.title}</a>
+                  </Link>
+                </td>
+              </tr>
+             ))}
+          </tbody>
+        </table>
+      </section>
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+  const posts = await getRecentPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};

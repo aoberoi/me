a0b86e2fb705cf82b54bc48723db44941ff70c90
Ankur Oberoi <aoberoi@gmail.com>
2022-01-28 15:00:29 -0800

    Simplify the interface to get recent posts
    This is a pretty significant refactor to the code that finds and builds post objects.
    This adds the concept of published and draft posts. It
    also reduces code duplication. And now posts are
    returned in a sorted order. The improved API is now used by the home page.

--- lib/post.ts
+++ lib/post.ts
@@ -1,4 +1,4 @@
-import type { JsonObject } from 'type-fest';
+import type { JsonObject, RequireAtLeastOne } from 'type-fest';
 import { join as pathJoin } from 'path';
 import { types } from 'util';
 import { readdir, readFile } from 'fs/promises';
@@ -6,15 +6,40 @@ import matter, { GrayMatterFile, Input } from 'gray-matter';
 import remark from 'remark';
 import html from 'remark-html';
 
+export enum PostStatus {
+  /** Visible on the site */
+  Published = 'published',
+  /** Hidden on the site */
+  Draft = 'draft',
+}
 
-export interface Post extends JsonObject {
+// Using several interfaces here to model the requirement that a published post requires a date, while other variations
+// of a post leaves date as optional.
+
+interface _PostBase extends JsonObject {
+  /** The last path component of the URL for this post. The slug is a unique identifier for the post. */
   slug: string;
   title: string;
-  date: number; // seconds since unix epoch
+  status: PostStatus;
+  /** Seconds since the unix epoch */
+  date?: number;
+  /** HTML content of the post */
   content: string;
 }
 
+export interface PublishedPost extends _PostBase {
+  status: PostStatus.Published;
+  date: number;
+}
+
+export type Post = PublishedPost | _PostBase;
+
 // This simple implementation only supports posts as `.md` files at the top level of the directory
+
+// There's several places where data is created by reading from the filesystem, and then recreated later by reading
+// again. We could improve performance by caching some of the data that's read in memory and reusing it. Not sure what
+// the hit rate of that cache might be if the next build is parallelized across many processes that cannot share the
+// cache.
 
 const postsDirectory = pathJoin(process.cwd(), '_posts');
 
@@ -29,16 +54,33 @@ function slugToFilename(slug: string): string {
 }
 
 async function fileToPost(file: GrayMatterFile<Input>, slug: string): Promise<Post> {
-  if (typeof file.data.title !== 'string' || !types.isDate(file.data.date)) {
+  if (typeof file.data.title !== 'string') {
     throw new Error('Invalid post');
   }
+
   const renderedContent = await markdownProcessor.process(file.content);
+  const date = file.data.date;
+
+  if (types.isDate(date)) {
+    return {
+      slug,
+      title: file.data.title,
+      status: PostStatus.Published,
+      date: Math.floor(date.getTime() / 1000),
+      content: renderedContent.toString(),
+    };
+  }
+
   return {
     slug,
     title: file.data.title,
-    date: Math.floor(file.data.date.getTime() / 1000),
+    status: PostStatus.Draft,
     content: renderedContent.toString(),
   };
+}
+
+function isPublished(post: Post): post is PublishedPost {
+  return post.status === PostStatus.Published;
 }
 
 export async function getAllSlugs(): Promise<string[]> {
@@ -46,27 +88,46 @@ export async function getAllSlugs(): Promise<string[]> {
   return filenames.map(filenameToSlug);
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+/**
+ * Retrieve the most recent `n` number of published posts.
+ *
+ * NOTE: Current callers of this function are only interested in the post metadata for listings. There a potential
+ * performance optimization available by skipping the markdown processing for the file contents.
+ *
+ * NOTE: This might be better modeled as a streaming operation, to support future streaming SSR. In this case, instead
+ * of `n` being a parameter, the entire stream could be subscribed and the caller could just take n from the stream and
+ * close it.
+ *
+ * @param n number of posts
+ */
+export async function getRecentPosts(n = Infinity): Promise<Post[]> {
   const filenames = await readdir(postsDirectory);
+  const postPromises = filenames.map((filename) => getPostFromLocation({ filename }));
 
-  const postPromises = filenames.map(async (filename) => {
-    const fullFilename = pathJoin(postsDirectory, filename);
-    const slug = filenameToSlug(filename);
-    const contents = await readFile(fullFilename, 'utf8');
-    const markdown = matter(contents);
-    return fileToPost(markdown, slug);
-  });
-
-  // Files that don't contain a valid post (such as a draft) are represented by a promise that rejects. Those are
-  // filtered out so that only valid posts are returned from this function.
-  return (await Promise.allSettled(postPromises))
+  const posts = (await Promise.allSettled(postPromises))
     .filter(r => r.status === 'fulfilled')
     .map(r => (r as PromiseFulfilledResult<Post>).value);
+  const publishedPosts = posts.filter(isPublished);
+  const sortedPosts = publishedPosts.sort((a, b) => a.date - b.date);
+
+  return sortedPosts.slice(0, n - 1);
 }
 
-export async function getPost(slug: string): Promise<Post> {
-  const fullFilename = pathJoin(postsDirectory, slugToFilename(slug));
+export type PostLocation = RequireAtLeastOne<{
+  filename?: string;
+  slug?: string;
+}, 'filename' | 'slug'>
+
+export async function getPostFromLocation(location: PostLocation): Promise<Post> {
+  // Ideally, the PostLocation type would support narrowing, but it does not.
+  // https://github.com/sindresorhus/type-fest/issues/230
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const filename = location.filename ?? slugToFilename(location.slug!);
+  const slug = location.slug ?? filenameToSlug(filename);
+
+  const fullFilename = pathJoin(postsDirectory, filename);
   const contents = await readFile(fullFilename, 'utf8');
-  const markdown = matter(contents);
-  return fileToPost(markdown, slug);
+
+  const vFile = matter(contents);
+  return fileToPost(vFile, slug);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import type { GetStaticProps } from 'next';
-import { Post, getAllPosts } from '../lib/post';
+import { Post, getRecentPosts } from '../lib/post';
 import { formatInTimeZone } from '../lib/date';
 import Layout from '../components/Layout';
 import Link from 'next/link';
 
 interface HomeProps {
-  posts: Post[];
+  recentPosts: Post[];
 }
 
-export default function Home({ posts }: HomeProps) {
+export default function Home({ recentPosts }: HomeProps) {
   return (
     <Layout>
       <section className="max-w-screen-lg mx-auto px-4">
         <h2 className="text-2xl font-banner my-2">Recent Posts</h2>
         <table className="w-full mx-0 mt-0 mb-4 p-0">
           <tbody>
-            {posts.map((post) => (
+            {recentPosts.map((post) => (
               <tr key={post.slug}>
                 <td className="pr-4 py-3 border-b border-gray-200/10 text-right opacity-40">{formatInTimeZone(post.date * 1000, 'MMMM do, R', 'UTC')}</td>
                 <td className="pl-4 py-3 border-b border-gray-200/10 text-left">
@@ -33,12 +33,11 @@ export default function Home({ posts }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  // A query for all recent posts
-  const posts = await getAllPosts();
+  const recentPosts = await getRecentPosts(3);
 
   return {
     props: {
-      posts,
+      recentPosts,
     },
   };
 };
